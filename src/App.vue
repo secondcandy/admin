047fc4d66e8ab698de10916a48059432477c54f4
nathan <1197590647@qq.com>
2023-08-06 16:25:00 +0800

    提交部分配置文件

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <div>
+  <div class="container">
     <h2>我是App根组件</h2>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
+<style lang="scss">
+.container {
+  background: lightblue;
+  width: 200px;
+  height: 200px;
+}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+const a: any = 100
